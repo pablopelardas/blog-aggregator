d0fb1fb0489af8bfdc54b57bee4bdaed895df48f
Pablo Pelardas <pablo.pelardas@gmail.com>
2024-03-18 18:52:29 -0300

    feat: Init database, helpers and middlewares modules

--- go.mod
+++ go.mod
@@ -2,8 +2,18 @@ module github.com/pablopelardas/blog-aggregator
 
 go 1.22.0
 
-require github.com/joho/godotenv v1.5.1 
+require github.com/joho/godotenv v1.5.1
+
 require internal/helpers v1.0.0
+
 replace internal/helpers => ./internal/helpers
+
 require internal/middlewares v1.0.0
+
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
+
 replace internal/middlewares => ./internal/middlewares
--- main.go
+++ main.go
@@ -1,30 +1,60 @@
 package main
 
 import (
+	"database/sql"
+	"internal/helpers"
 	"log"
 	"net/http"
 	"os"
 
-	"internal/helpers"
-	"internal/middlewares"
-
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/pablopelardas/blog-aggregator/internal/database"
 )
+
+type apiConfig struct{
+	DB *database.Queries
+}
 
 func main(){
 	godotenv.Load()
 	port := os.Getenv("PORT")
-	r := http.NewServeMux()
-	corsMux := middlewares.Cors(r)
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbQueries := database.New(db)
+	apiConfig := &apiConfig{
+		DB: dbQueries,
+	}
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		helpers.RespondWithJSON(w, http.StatusOK, map[string]string{"message": "Hello, World!"})
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		helpers.RespondWithJSON(w, http.StatusOK, map[string]string{"status": "ok"})
 	})
+	v1Router.Get("/err", func(w http.ResponseWriter, r *http.Request) {
+		helpers.RespondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+	})
+
+	router.Mount("/v1", v1Router)
 
 	// Start the server
 	srv := &http.Server{
 		Addr:    ":" + port,
-		Handler: corsMux,
+		Handler: router,
 	}
 	log.Print("Server started at port " + port)
 	log.Fatal(srv.ListenAndServe())
